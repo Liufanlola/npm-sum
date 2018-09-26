97f210fd3c1938585dd59aa07b9e7932b49339c4
lola liu <18068001070@163.com>
2018-09-26 16:47:17 +0800

    求和函数导出修改

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-exports.sum = function () {
+function sum () {
     return Array.from(arguments).reduce( (accumulator,currentValue) => {
         return accumulator + currentValue
     })
 }
+module.exports = sum
